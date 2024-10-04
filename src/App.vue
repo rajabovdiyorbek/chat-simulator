64cea546afa3188d39479b626502d4796cdb9b04
Diyorbek Rajabov <diyorbek@abdigital.uz>
2024-10-04 21:14:23 +0500

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>Counter: {{ store.counter }}</p>
+    <button @click="store.increment">Increment</button>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { useMainStore } from "./stores/store";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const store = useMainStore();
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
